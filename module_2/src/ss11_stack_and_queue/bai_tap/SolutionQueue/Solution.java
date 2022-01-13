e68f5462b1ccc2da71c391a580ca87c19481a302
C1121G1-NguyenHuuLeLong <long.nguyen211996@gmail.com>
2022-01-13 15:45:05 +0700

    update

--- module_2/src/ss11_stack_and_queue/bai_tap/SolutionQueue/Solution.java
+++ module_2/src/ss11_stack_and_queue/bai_tap/SolutionQueue/Solution.java
@@ -6,38 +6,38 @@ public class Solution {
         private Node front;
         private Node rear;
 
+    }
 
-        public class Node {
-            private int data;
-            private Node link;
+    public class Node {
+        private int data;
+        private Node link;
 
+    }
+
+    public void enQueue(Queue queue, int data) {
+        Node newNode = new Node();
+        if (queue.front == null) {
+            newNode = queue.front;
+        } else {
+            newNode = queue.rear.link;
         }
+        newNode = queue.rear;
+        queue.rear.link = queue.front;
 
-        public void enQueue(Queue queue, int data) {
-            Node newNode = new Node();
-            if (queue.front == null) {
-                newNode = queue.front;
-            } else {
-                newNode = queue.rear.link;
-            }
-            newNode = queue.rear;
+    }
+
+    public void deQueue(Queue queue) {
+        int data;
+        if (queue.front == null) {
+            System.out.println(" Queue is empty ");
+        } else if (queue.front == queue.rear) {
+            data = queue.front.data;
+            queue.rear = null;
+            queue.front = null;
+        } else {
+            data = queue.front.data;
+            queue.front = queue.front.link;
             queue.rear.link = queue.front;
-
-        }
-
-        public void deQueue(Queue queue) {
-            int data;
-            if (queue.front == null) {
-                System.out.println(" Queue is empty ");
-            } else if (queue.front == queue.rear) {
-                data = queue.front.data;
-                queue.rear= null;
-                queue.front=null;
-            } else {
-                data = queue.front.data;
-                queue.front = queue.front.link;
-                queue.rear.link =queue.front;
-            }
         }
     }
 }
