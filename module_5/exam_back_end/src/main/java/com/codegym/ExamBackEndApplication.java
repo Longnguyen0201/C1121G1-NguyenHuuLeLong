51acf1f6b5e0237f05e6aeae0cca3ff80fad0a54
C1121G1-NguyenHuuLeLong <long.nguyen211996@gmail.com>
2022-11-15 19:38:01 +0700

    deploy

--- module_5/exam_back_end/src/main/java/com/codegym/ExamBackEndApplication.java
+++ module_5/exam_back_end/src/main/java/com/codegym/ExamBackEndApplication.java
@@ -2,8 +2,10 @@ package com.codegym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ExamBackEndApplication {
 
     public static void main(String[] args) {
